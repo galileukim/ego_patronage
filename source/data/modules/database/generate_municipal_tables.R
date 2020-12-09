5c0e84be812c2e6b709afb68131b10ec10ad85e3
galileu.kim <galileukim@gmail.com>
2020-12-09 12:18:53 -0500

    create script to generate municipal tables

--- source/data/modules/database/generate_municipal_tables.R
+++ source/data/modules/database/generate_municipal_tables.R
@@ -3,7 +3,7 @@
 # output: municipal level table of summary statistics
 # requirement: reduce query times by generating readily available tables
 # ==============================================================================
-debug <- TRUE
+debug <- FALSE
 RSQLite::initExtension(rais_con)
 vars_to_plot <- c("hired", "fired", "age", "edu", "work_experience")
 
@@ -11,38 +11,57 @@ source(
     here::here("source/data/modules/database/requirements.R")
 )
 
-rais <- tbl(rais_con, "rais")
-filiado <- tbl(rais_con, "filiado_mun")
-
-rais_partisan <- rais %>%
-    anti_join(filiado) %>%
-    group_by(
-        cod_ibge_6, year
-    ) %>%
-    summarise(
-        across(
-            vars_to_plot,
-            list(mean = mean, median = median),
-            na.rm = TRUE
-        )
+sprintf(
+    "DROP TABLE IF EXISTS %s", c("rais_mun_partisan", "rais_mun_non_partisan")
+) %>%
+    walk(
+        dbExecute,
+        conn = rais_con
     )
 
-rais_partisan <- dbGetQuery(
+dbExecute(
     rais_con,
     "
+    CREATE TABLE IF NOT EXISTS
+    rais_mun_non_partisan AS
     SELECT
     rais.cod_ibge_6,
     rais.year,
+    SUBSTR(rais.cbo_02, 1, 1) AS occupation,
     AVG(hired) AS hired,
     AVG(fired) AS fired,
     AVG(age) AS age,
     AVG(EDU) AS edu,
     AVG(work_experience) AS work_experience,
-    MEDIAN(wage) AS wage
+    MEDIAN(wage) AS wage,
+    COUNT(*) AS n
     FROM rais
     LEFT JOIN filiado_mun
     ON rais.id_employee = filiado_mun.id_employee
     WHERE filiado_mun.id_employee IS NULL
-    GROUP BY rais.cod_ibge_6
+    GROUP BY rais.cod_ibge_6, rais.year, occupation
+    "
+)
+
+dbExecute(
+    rais_con,
+    "
+    CREATE TABLE IF NOT EXISTS
+    rais_mun_partisan AS
+    SELECT
+    rais.cod_ibge_6,
+    rais.year,
+    SUBSTR(rais.cbo_02, 1, 1) AS occupation,
+    AVG(hired) AS hired,
+    AVG(fired) AS fired,
+    AVG(age) AS age,
+    AVG(EDU) AS edu,
+    AVG(work_experience) AS work_experience,
+    MEDIAN(wage) AS wage,
+    COUNT(*) AS n
+    FROM rais
+    INNER JOIN filiado_mun
+    ON rais.id_employee = filiado_mun.id_employee
+    GROUP BY rais.cod_ibge_6, rais.year, occupation
     "
 )
