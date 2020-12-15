c272f016f75ea5d37b0bf4e010f2d42b267783ee
galileu.kim <galileukim@gmail.com>
2020-12-15 11:29:34 -0500

    visualization for employed vs unemployed

--- source/descriptive/modules/party_membership.R
+++ source/descriptive/modules/party_membership.R
@@ -8,11 +8,14 @@ source(
     here::here("source/descriptive/modules/requirements.R")
 )
 
+# ==============================================================================
+# generate descriptive statistics of party members
+# ==============================================================================
 rais_sql <- tbl(rais_con, "rais")
 filiado_sql <- tbl(rais_con, "filiado_mun")
 
 rais_tables <- c(
-    partisan = "rais_mun_partisan", 
+    partisan = "rais_mun_partisan",
     non_partisan = "rais_mun_non_partisan"
 )
 
@@ -23,43 +26,49 @@ rais_filiado <- map_dfr(
 )
 
 # create entries and exits per year
-filiado <- filiado_sql %>%
-    collect() %>%
-    mutate(
-        across(starts_with("date"), lubridate::ymd)
-    )
-
-filiado_turnover <- filiado %>% 
-    pivot_longer(
-        c(date_start, date_end),
-        names_prefix = "date_",
-        names_to = "date"
-    ) %>%
-    group_by(date, value) %>%
-    summarise(n = n())
-
-# verify when data for membership termination begins
-filiado_turnover %>%
-    ggplot() +
-    geom_point(
-        aes(x = value, y = n)
-    ) +
-    geom_vline(xintercept = 
-        as.numeric(as.Date(sprintf("%s-01-01", seq(1997, 2013, 4))))
-    ) +
-    facet_wrap(. ~ date) +
-    coord_cartesian(ylim = c(0, 50)) +
-    scale_x_date(limit = as.Date(c("2003-01-01", "2019-01-01")))
-
-
-# compare duration of party membership between employed vs. unemployed
-filiado_employed <- filiado_sql %>%
-    inner_join(
+# segmented by employment
+filiado_unemployed <- filiado_sql %>%
+    anti_join(
         rais_sql %>%
-            select(),
+            select(cod_ibge_6, id_employee),
         by = c("cod_ibge_6", "id_employee")
     ) %>%
     collect()
 
-filiado_employed <- filiado_employed %>%
-    
+filiado_employed <- filiado_sql %>%
+    inner_join(
+        rais_sql %>%
+            select(cod_ibge_6, id_employee),
+        by = c("cod_ibge_6", "id_employee")
+    ) %>%
+    collect() %>%
+    distinct(filiado_id, .keep_all = TRUE)
+
+# compare duration of party membership between employed vs. unemployed
+filiado_turnover <- list(
+    filiado_employed,
+    filiado_unemployed
+) %>%
+    set_names(c("employed", "unemployed")) %>%
+    map_dfr(
+        . %>%
+            convert_to_date() %>%
+            pivot_longer(
+                c(date_start, date_end),
+                names_prefix = "date_",
+                names_to = "date"
+            ) %>%
+            count(date, value),
+        .id = "is_employed"
+    )
+
+# verify when data for membership termination begins
+filiado_turnover %>%
+    gg_point(
+        aes(x = value, y = n),
+        size = 2
+    ) +
+    plot_mandate_year() +
+    facet_wrap(. ~ date + is_employed) +
+    coord_cartesian(ylim = c(0, 50)) +
+    scale_x_date(limit = as.Date(c("2003-01-01", "2019-01-01")))
