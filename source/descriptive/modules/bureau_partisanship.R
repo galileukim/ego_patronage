3bc31b0a37fd35e1ca2086682b9ed1bbe2f11f78
galileu.kim <galileukim@gmail.com>
2020-12-16 10:31:35 -0500

    begin mapping out partisanship

--- source/descriptive/modules/bureau_partisanship.R
+++ source/descriptive/modules/bureau_partisanship.R
@@ -10,7 +10,15 @@ source(
 
 rais_filiado <- dbGetQuery(
     "
-    SELECT * FROM rais 
+    SELECT 
+        rais.cod_ibge_6,
+        rais.cbo_02,
+        rais.cnae_95,
+        rais.wage,
+        rais.contract_type,
+        filiado_mun.date_start,
+        filiado_mun.date_end
+    FROM rais
     LEFT JOIN filiado_mun
     ON rais.cod_ibge_6 = filiado_mun.cod_ibge_6 AND
     rais.id_employee = filiado_mun.id_employee
